543590700ab5f1adc3531d7fb933c505ef55a9fb
ninad555 <kadamninad007@gmail.com>
2021-08-07 23:28:46 +0530

    app update

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
+import csv
+import pandas as pd
 
 app = Flask(__name__)
+
+
+def CreateNewFile():
+    fieldnames = ['Name', "last_name", "Age"]
+    with open("details.csv", 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+
+def AppendFile(dct):
+    fieldnames = ['Name', "last_name", "Age"]
+    with open("details.csv", 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerows(dct)
+
+
+new_file = CreateNewFile()
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -8,8 +26,9 @@ def details():
     if request.method == "POST":
         user_name = request.form["name"]
         user_last_name = request.form["sur-name"]
-
-        print(user_name, user_last_name)
+        user_age = request.form["age"]
+        dct =[{'Name': user_name, "last_name":user_last_name, "Age": user_age}]
+        AppendFile(dct)
 
         return render_template("forms.html")
     else:
